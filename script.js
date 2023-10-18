dcfd5292d34baf29583bcaf1717edd401bf1f2bc
hamihams <nolihi109@gmail.com>
2023-10-18 14:13:27 -0700

    all js events

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ let z = 0;
 function myMoveFunction() {
     document.getElementById("heart").innerHTML = z+=1;
   }
+
+  function mouseOver() {
+    document.getElementById("one").style.color = "orange";
+  }
+  
+  function mouseOut() {
+    document.getElementById("one").style.color = "blue";
+  }
+
+
+  
+setInterval(myTimer, 1000);
+
+function myTimer() {
+  const date = new Date();
+  document.getElementById("time").innerHTML = date.toLocaleTimeString();
+}
